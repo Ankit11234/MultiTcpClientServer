483fb541d59118043a7ce73c418af6a7bb553fde
Ankit11234 <ankittripathi13486@gmail.com>
2023-12-26 19:33:09 +0530

    initial commit

---
+++ client/MultiClientTcpClient/MultiClientTcpClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MultiClientTcpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string clientName = Console.ReadLine();
+
+            using (TcpClient clientSocket = new TcpClient())
+            {
+                clientSocket.Connect("127.0.0.1", 8888);
+                Console.WriteLine($"Connected to the server.");
+
+                try
+                {
+                    using (NetworkStream serverStream = clientSocket.GetStream())
+                    {
+                        while (true)
+                        {
+                            Console.Write($"{clientName}: ");
+                            string message = Console.ReadLine();
+                            if (string.IsNullOrEmpty(message))
+                                continue;
+
+                            byte[] outStream = Encoding.ASCII.GetBytes($"{clientName}: {message}\0");
+                            serverStream.Write(outStream, 0, outStream.Length);
+                            serverStream.Flush();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ server/MultiClientTcpServer/MultiClientTcpServer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiClientTcpServer
+{
+    internal class Program
+    {
+        private static TcpListener serverSocket;
+        private static List<TcpClient> clients = new List<TcpClient>();
+
+        static async Task Main(string[] args)
+        {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+            serverSocket = new TcpListener(ipAd, 8888);
+            serverSocket.Start();
+            Console.WriteLine("***********Server Started *********");
+
+            while (true)
+            {
+                TcpClient clientSocket = await serverSocket.AcceptTcpClientAsync();
+                clients.Add(clientSocket);
+
+                Console.WriteLine($"Accepted connection from client {clients.Count}");
+
+                Task.Run(() => HandleClient(clientSocket));
+            }
+        }
+
+        private static async Task HandleClient(TcpClient clientSocket)
+        {
+            try
+            {
+                using (NetworkStream networkStream = clientSocket.GetStream())
+                {
+                    byte[] bytesFrom = new byte[10025];
+
+                    while (true)
+                    {
+                        int bytesRead = await networkStream.ReadAsync(bytesFrom, 0, bytesFrom.Length);
+                        if (bytesRead == 0)
+                        {
+                            // If bytesRead is 0, the client has disconnected
+                            Console.WriteLine($"Client {clientSocket.Client.RemoteEndPoint} disconnected.");
+                            break;
+                        }
+
+                        string dataFromClient = Encoding.ASCII.GetString(bytesFrom, 0, bytesRead);
+                        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf('\0'));
+
+                        Console.WriteLine($"Data from client {clientSocket.Client.RemoteEndPoint}: {dataFromClient}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error handling client {clientSocket.Client.RemoteEndPoint}: {ex.Message}");
+            }
+            finally
+            {
+                clients.Remove(clientSocket);
+                clientSocket.Close();
+            }
+        }
+    }
+}
